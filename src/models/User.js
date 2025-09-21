bf12cdafad53061e099a7ea9a7ba1d57f6b2b925
Krish Dambir <krishdambir2718@gmail.com>
2025-09-21 16:21:45 +0530

    token handling2b

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const UserSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
@@ -11,15 +11,17 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-UserSchema.pre("save", async function (next) {
+// Hash password before saving
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-UserSchema.methods.matchPassword = async function (entered) {
-  return bcrypt.compare(entered, this.password);
+// Compare password method
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
-export default mongoose.model("User", UserSchema);
+export default mongoose.model("User", userSchema);
